d32c49827374a3ce71237955919dad5babbe17dd
pecodrive <pecodrive@gmail.co>
2016-10-04 11:32:12 +0900

    cssfix

--- outline.php
+++ outline.php
@@ -4,7 +4,7 @@ $flag = preg_match_all("/<(h[1-6]{1}).*>(.*)<\/h[1-6]{1}>/u", $content, $htag, P
 
 if($flag === 0){
 }else{
-    echo '<div class="outline">';
+    echo '<div id="outline">';
     echo '<div id="outline__title">目次</div>';
     echo '<ul id="outline__ul">';
     $h1Flag = $h2Flag = $h3Flag = $h4Flag = $h5Flag = $h6Flag = 0;
